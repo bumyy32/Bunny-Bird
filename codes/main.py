456a9f76c3c632606752327f87445f12647942ec
bumyy <bumyy32@gmail.com>
2022-11-30 01:04:35 -0300

    basic setup done
    added sprites group

--- codes/main.py
+++ codes/main.py
@@ -1,25 +1,30 @@
 import pygame, sys, time
 from settings import *
 
+
 class Game:
 
     def __init__(self):
 
-    # Setup
+        # Setup
         pygame.init()
-        self.display_surface = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
+        self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
         pygame.display.set_caption("Bunny Bird!")
         self.clock = pygame.time.Clock()
+
+        # Sprite groups
+        self.all_sprites = pygame.sprit.Group()
+        self.collision_sprites = pygame.sprit.Group()
 
     def run(self):
         last_time = time.time()
         while True:
 
-            #DeltaTime
+            # DeltaTime
             dt = time.time() - last_time
             last_time = time.time()
 
-            #event loop
+            # event loop
             for event in pygame.event.get():
                 if event.type == pygame.QUIT():
                     pygame.quit()
@@ -29,7 +34,7 @@ class Game:
             pygame.display.update()
             self.clock.tick(FRAMERATE)
 
+
 if __name__ == '__main__':
     game = Game()
     game.run()
-
